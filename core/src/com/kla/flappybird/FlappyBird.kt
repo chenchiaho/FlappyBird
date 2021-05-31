0c8c3fcc3c32911d3d2751679a55a3cd1be56d63
Kevin La <chenchiaho@gmail.com>
2021-05-31 14:37:19 +0800

    set logic for bird dropping and moving up by tapping

--- core/src/com/kla/flappybird/FlappyBird.kt
+++ core/src/com/kla/flappybird/FlappyBird.kt
@@ -12,6 +12,10 @@ class FlappyBird : ApplicationAdapter() {
 
     var birds = arrayOfNulls<Texture?>(2)
     var flapState = 0
+    var birdY = 0f
+    var velocity = 0
+    var gameState = 0
+    var gravity = 2
 
 
     override fun create() {
@@ -21,16 +25,35 @@ class FlappyBird : ApplicationAdapter() {
 //        birds = arrayOfNulls<Texture>(2)
         birds[0] = Texture("bird.png")
         birds[1] = Texture("bird2.png")
+        birdY = Gdx.graphics.height.toFloat() / 2 - birds[0]!!.height / 2
 
     }
 
     override fun render() {
 
-        flapState = if (flapState == 0) {1} else 0
+        if (gameState != 0) {
+
+            if (Gdx.input.justTouched()) {
+                velocity = -30
+            }
+
+            if (birdY > 0 || velocity < 0) {
+                velocity += gravity
+                birdY -= velocity
+            }
+        } else {
+            if (Gdx.input.justTouched()) {
+                gameState = 1
+            }
+        }
+
+        flapState = if (flapState == 0) {
+            1
+        } else 0
 
         batch!!.begin()
         batch!!.draw(background, 0f, 0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
-        batch!!.draw(birds[flapState], Gdx.graphics.width.toFloat() / 2 - birds[flapState]!!.width / 2, Gdx.graphics.height.toFloat() / 2 - birds[flapState]!!.height / 2)
+        batch!!.draw(birds[flapState], Gdx.graphics.width.toFloat() / 2 - birds[flapState]!!.width / 2, birdY)
         batch!!.end()
     }
 }
